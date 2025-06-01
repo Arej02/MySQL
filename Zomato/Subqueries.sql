f738ba9d9aabcab63d62522a1965b0913574bead
Arej02 <159859689+Arej02@users.noreply.github.com>
2025-06-01 17:13:28 +0930

    Subqueries Practise

--- Zomato/Subqueries.sql
+++ Zomato/Subqueries.sql
@@ -29,3 +29,23 @@ WHERE star in (SELECT star FROM movies
 WHERE votes>25000
 GROUP BY star
 HAVING AVG(score)>8.5);
+
+-- 4) Find the most profitable movie of each year:
+SELECT * FROM movies 
+WHERE (year,gross-budget) IN (SELECT year,MAX(gross-budget) 
+FROM movies GROUP BY year);
+
+-- 5)Find the highest rated movies of each genre votes cutoff of
+-- 25000
+SELECT * FROM movies
+WHERE (genre,score) IN
+(SELECT genre,MAX(score) FROM movies
+WHERE votes>25000
+GROUP BY genre) AND votes>25000;
+
+-- 6)Find the highest grossing movies of top 5 actor/director combo
+-- in terms of total gross income 
+SELECT * FROM movies
+WHERE (star,gross) IN
+(SELECT star,SUM(gross) FROM movies
+GROUP BY star ORDER BY SUM(gross) DESC LIMIT 5);
